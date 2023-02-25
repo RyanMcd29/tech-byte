f94b5f607a864ab835991e38cb0bbc3f3876c881
RyanMcd29 <117286428+RyanMcd29@users.noreply.github.com>
2023-02-25 17:58:41 +0800

    mapped the relationships of each model

--- models/index.js
+++ models/index.js
@@ -1,16 +1,31 @@
 const Blog_Posts = require('./User');
 const User = require('./User')
+const Comment = require('./Comment')
 
 User.hasMany(Blog_Posts, {
-    foreignKey: 'user_id'
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE',
 });
 
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+})
+
 User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE',
+})
+
+Comment.belongsTo(User, {
     foreignKey: 'user_id'
 })
 
 Post.hasMany(Comment, {
-    foreignKey: 'comment_id'
+    foreignKey: 'comment_id',
+    onDelete: 'CASCADE',
 })
+
+
+
 
 module.exports = { Blog_Posts, User };
