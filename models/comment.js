571be19ffea9428d4cd6c7b0c3e3c4e8499e225b
RyanMcd29 <117286428+RyanMcd29@users.noreply.github.com>
2023-02-25 17:40:13 +0800

    add comment model

--- models/comment.js
+++ models/comment.js
@@ -2,9 +2,9 @@ const { Model, Datatypes } = require('sequelize');
 const bcrypt = require('bcrypt')
 const sequelize = require('../config/connection');
 
-class Blog_Posts extends Model {}
+class Comment extends Model {};
 
-Blog_Posts.init(
+Comment.init(
     {
         id: {
             type: Datatypes.INTEGER,
@@ -12,17 +12,9 @@ Blog_Posts.init(
             primaryKey: true,
             autoIncrement: true,
         },
-        title: {
-            type: Datatypes.STRING,
-            allowNull: false,
-        },
         body: {
             type: Datatypes.STRING,
-            allowNul: false,
-        },
-        time_created: {
-            type: Datatypes.DATE,
-            allowNull: false,
+            allowNull: false
         },
         user_id: {
             type: Datatypes.INTEGER,
@@ -30,21 +22,14 @@ Blog_Posts.init(
                 model: 'user',
                 key: 'id',
             }
-        },
-        comment_id: {
-            type: Datatypes.INTEGER,
-            references: {
-                model: 'comment',
-                key: 'id'
-            }
         }
     },
     {
         sequelize,
         freezeTableName: true,
         underscored: true,
-        modelName: 'blog_posts'
+        modelName: 'comment'
     }
-)
+);
 
-module.exports = Blog_Posts
+module.exports = Comment;
