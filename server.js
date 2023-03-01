a50ce6b4dfd1eccaf8a8cf2f5beea9f8f98aca74
RyanMcd29 <117286428+RyanMcd29@users.noreply.github.com>
2023-03-01 20:21:51 +0800

    fix typo in server

--- server.js
+++ server.js
@@ -1,31 +1,32 @@
 const path = require('path');
 const express = require('express');
-// const session = require('express-session');
-const exphbs = require('express-handlebars');
+const session = require('express-session');
+const exphbs = require('express-handlebars')
 const routes = require('./controllers');
-// const helpers = require('./utils/helpers');
+const helpers = require('./utils/helpers');
+
 const sequelize = require('./config/connection');
-// const SequelizeStore = require('connect-session-sequelize')(session.Store);
-const hbs = exphbs.create({});
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
- 
+
+const hbs = exphbs.create({ helpers });
 
 
-// const sess = {
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store : new SequelizeStore({
+        db: sequelize
+    })
+};
 
-//     cookie: {},
-//     resave: false,
-//     saveUninitialized: true,
-//     store : new SequelizeStore({
-//         db: sequelize
-//     })
-// };
+app.use(session(sess));
 
-// app.use(session(sess));
-
-app.engine('handlebars'. hbs.engine);
+app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
