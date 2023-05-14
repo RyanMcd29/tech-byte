71b74921ccd7be443de374e18b00bfaf379eff86
RyanMcd29 <117286428+RyanMcd29@users.noreply.github.com>
2023-05-14 22:09:57 +0800

    add handler to delete post from profile

--- controllers/api/post.js
+++ controllers/api/post.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const { Post } = require('../../models');
 const withAuth = require('../../utils/auth')
 
+// Add Post
 router.post('/', withAuth, async (req, res) => {
     try {
         const newPost = await Post.create({
@@ -14,3 +15,26 @@ router.post('/', withAuth, async (req, res) => {
         res.status(400).json(err);
     }
 })
+
+// Delete Post
+router.delete('/:id', withAuth, async(req, res) => {
+    try {
+        const postData = await Post.destroy({
+            where: {
+                id: req.params.id,
+                user_id: req.session.user_id
+            },
+        })
+
+        if (!postData) {
+            res.status(404).json({ message: "No post with this ID found"});
+            return;
+        }
+
+        res.status(200).json(postData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
--- public/js/profile.js
+++ public/js/profile.js
@@ -23,6 +23,28 @@ const newPostHandler = async (event) => {
     }
 }
 
+
+const deleteBtnHandler = async (event) => {
+    if (event.target.hasAttribute('data-id;')) {
+        const id = event.target.getAttribute('data-id');
+
+        const response = await fetch(`api/posts/${id}`, {
+            method: 'DELETE',
+        })
+
+        if (response.ok) {
+            document.location.replace('/profile');
+        } else {
+            alert('Failed to delete post')
+        }
+    }
+};
+
+
 document
     .querySelector('.new-post-form')
     .addEventListener('submit', newPostHandler)
+
+document
+    .querySelector('.post-list')
+    .addEventListener('click', deleteBtnHandler)
