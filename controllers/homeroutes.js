ac6cea02140648fc5350559726e17ba0473e751c
RyanMcd29 <117286428+RyanMcd29@users.noreply.github.com>
2023-05-15 19:33:02 +0800

    cleanup code

--- controllers/homeroutes.js
+++ controllers/homeroutes.js
@@ -79,10 +79,11 @@ router.get('/post/:id', async (req, res) => {
 
         const post = postData.get({ plain: true });
 
-        res.render('post', {
+        res.render("post", {
             ...post,
             logged_in: req.session.logged_in
         });
+        
     } catch (err) {
         res.status(500).json(err)
     }
